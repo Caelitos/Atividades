3e9e0a4273ef734fc7525ba5154269136ae32059
Carlos Antonio <42525177+Caelitos@users.noreply.github.com>
2018-09-19 19:11:47 -0300

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,42 +1,31 @@
 //CAAC
 package atividades;
-
 import java.util.Scanner;
 
-
 public class Fibonacci {
-  
-    
-  
-  
+
     public static void main(String[] args) {
     Scanner tcl = new Scanner(System.in);
-        System.out.println("Digite até que posição quer pesquisar:");
-        int tam = tcl.nextInt(); 
-        long a = 1;
-        long b = 0;
-        long aux;
-        
-        long vetor[] = new long[tam];
-        
-        for (int i = 0; i < vetor.length; i++) {
-            vetor[i] = b;
-            aux = a;
-            a += b;
-            b = aux;
-            System.out.println(vetor[i]);
-                    
-        }
-        
-      
-        System.out.println("Digite a posição da sequência");
-        int nume = tcl.nextInt();
-        System.out.println("");
-        for (int j = 0; j < vetor.length; j++) {
-            if(nume == ( j + 1 ) ) {
-               System.out.println("Numero da posição: " + (j + 1) + "° é " + vetor[j]);
+    int tam = tcl.nextInt();
 
+    long vet[] = new long[tam];
+        for (int i = 0; i < vet.length; i++) {
+           if (i < 2){
+               vet[i] = i;
+           }else{
+               vet[i] = vet[i - 1] + vet[i - 2];
+           }
+            System.out.println(vet[i]);
+            
+        }
+        System.out.println("Digite o valor a ser procurado");
+        int nume = tcl.nextInt();
+        for (int j = 0; j < vet.length; j++) {
+            if(nume == j) {
+               System.out.println("Numero encontrado " + j +"° :"+vet[j] ) ;
+                
             }
+    
         }
     }
 }  
